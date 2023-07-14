17ac497d7728cad987b3656d6be7805b2bd9f43b
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-07-14 06:52:29 +0530

    minor(video-section): added video section component

--- src/components/AppLayout/Main/index.tsx
+++ src/components/AppLayout/Main/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderSection from "@/components/HeaderSection";
 import FormSection from "@/components/FormSection";
+import VideoSection from "@/components/VideoSection";
 
 function Main(): JSX.Element {
   return (
     <main className="flex-1">
       <HeaderSection />
       <FormSection />
+      <VideoSection />
     </main>
   );
 }
