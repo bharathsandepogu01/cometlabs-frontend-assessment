52e396fc7a12ae6b7d5d6482b4fff9b81567cd83
bharathsandepogu01 <bharathsandepogu01@gmail.com>
2023-07-14 06:38:16 +0530

    minor(styles): tailwind custom style configuration and theme constants added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "header-section": "url('/images/header-section-bg-image.jpg')",
+      },
+      colors: {
+        primaryColor: {
+          light: "#e8fbfb",
+          DEFAULT: "#00c2c5",
+        },
+        textColor: {
+          primary: {
+            day: "#152f2e",
+            night: "#ffffff",
+          },
+          secondary: {
+            day: "#90adac",
+            night: "#ffffff",
+          },
+        },
+        backgroundColor: {
+          day: "#ffffff",
+          night: "#000000",
+          primary: "#152f2e",
+          card: {
+            day: "#ebebeb",
+            night: "#1f2028",
+          },
+        },
+        error: "#ff3333",
+        borderColor: {
+          DEFAULT: "#d4d4d4",
+        },
       },
     },
   },
+  darkMode: "class",
   plugins: [],
-}
+};
